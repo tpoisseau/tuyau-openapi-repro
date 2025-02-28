e295180a57a3a3fce3d7ba75632c349c00a1714f
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2025-02-28 11:21:52 +0100

    feat: illustrate issues default servers list

--- config/tuyau.ts
+++ config/tuyau.ts
@@ -3,6 +3,20 @@ import { defineConfig } from '@tuyau/core'
 const tuyauConfig = defineConfig({
   openapi: {
     provider: 'swagger-ui',
+    exclude: ['/wildcard/:category/:id/*'], // exclude bugged route schema
+    documentation: {
+      info: {
+        title: 'AdonisJS API',
+        version: '1.0.0',
+      },
+      // uncomment to fix the servers list
+      // servers: [
+      //   {
+      //     url: '/',
+      //     description: 'Development server',
+      //   },
+      // ],
+    },
   },
   codegen: {
     /**
--- inertia/tuyau.ts
+++ inertia/tuyau.ts
@@ -3,7 +3,7 @@ import { createTuyau } from '@tuyau/client'
 
 export const tuyau = createTuyau({
   api,
-  baseUrl: 'http://localhost:3333',
+  baseUrl: '/',
 })
 
 async function testTypes() {
